9a27333c0a35980d9d8fb1f74737cf05f7cd83bc
张晓辉 <cococandoit@163.com>
2019-07-18 17:40:24 +0800

    add bi-directional data binding

--- extend/index.js
+++ extend/index.js
@@ -4,6 +4,14 @@ const preComponent=Component;
 
 Page=function(option){
   option.validate=new Validate();
+  // 双向绑定扩展
+  option.assignValue=function(e){
+    console.log(e);
+    let name=e.target.dataset.name;
+    if(name) this.setData({
+      [name]:e.detail.value
+    });
+  };
   return prePage(option);
 }
 Component = function (option) {
