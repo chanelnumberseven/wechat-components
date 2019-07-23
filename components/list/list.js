b30c56b904d55ea5c5cf0582c11bcd464426abca
张晓辉 <cococandoit@163.com>
2019-07-23 15:00:14 +0800

    update list component

--- components/list/list.js
+++ components/list/list.js
@@ -1,10 +1,21 @@
 Component({
   properties:{
+    css:{
+      type:String
+    },
     list:{
       type:[Array,Object]
     },
     valueWidth:{
       type:String
+    }
+  },
+  methods:{
+    tap:function(e){
+      debugger
+      let event=e.currentTarget.dataset.event;
+      if(!event) return;
+      this.triggerEvent(event);
     }
   }
 })
